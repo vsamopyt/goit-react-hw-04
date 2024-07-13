b5b762cbb6a87cbc3d7ef27010d8ec4a82a05a48
v.samopyt <v.samopyt14@gmail.com>
2024-07-13 18:53:35 +0300

    commit-02

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import css from './App.module.css';
 import SearchBar from '../SearchBar/SearchBar';
 import ImageGallery from '../ImageGallery/ImageGallery';
 import BarLoader from "react-spinners/BarLoader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import {fetchImagesByTopic} from "../../images-api"
 
 function App() {
 
@@ -16,35 +18,70 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [error, setError] =useState(false)
 
+
+
+//   useEffect(() => {
+
+
+//     async function fetchImages() {
+//       try {
+//         setLoading(true);
+
+//         const accesKey = 'cZ-MFVyM7oimReB-5_cwTBy1PuN0gxZ0UbQfuk-h5hY';
+//         const perPage = 5;
+//         axios.defaults.headers.common[
+//           'Authorization'
+//         ] = `Client-ID ${accesKey}`;
+//         axios.defaults.headers.common['Accept-Version'] = `1`;
+
+//         const response = await axios.get(
+//           `https://api.unsplash.com/search/photos`,
+//           {
+//             params: {
+//               query: topic,
+//               per_page: perPage,
+//               orientation: 'landscape',
+//             },
+//           }
+//         );
+       
+
+//         setlistImages(response.data.results);
+        
+//       } catch (error) {
+        
+//         console.log("Error",error);
+//         setError(true)
+//       } finally {
+//         setLoading(false);
+//       }
+//     }
+    
+// if ( topic !== "") {
+//   setlistImages([])
+//   fetchImages();
+// }
+// else {
+//   return
+// }
+//   }, [topic]);
+
+
+
+// VARIANT 2 HTTP request on useEffeect mont. un click
+
   useEffect(() => {
+
+
     async function fetchImages() {
       try {
         setLoading(true);
 
-        const accesKey = 'cZ-MFVyM7oimReB-5_cwTBy1PuN0gxZ0UbQfuk-h5hY';
-        const perPage = 5;
-        axios.defaults.headers.common[
-          'Authorization'
-        ] = `Client-ID ${accesKey}`;
-        axios.defaults.headers.common['Accept-Version'] = `1`;
+        const data = await fetchImagesByTopic(topic);
+        console.log(data);
 
-        const response = await axios.get(
-          `https://api.unsplash.com/search/photos`,
-          {
-            params: {
-              query: topic,
-              per_page: perPage,
-              orientation: 'landscape',
-            },
-          }
-        );
-        console.log(topic);
-
-        console.log(response.data.results);
-        console.log(response);
-
-        setlistImages(response.data.results);
-        console.log('sssss', response.data.results);
+        setlistImages(data);
+        
       } catch (error) {
         
         console.log("Error",error);
@@ -53,50 +90,33 @@ function App() {
         setLoading(false);
       }
     }
-
-    fetchImages();
+    
+if ( topic !== "") {
+  setlistImages([])
+  fetchImages();
+}
+else {
+  return
+}
   }, [topic]);
+
+
+  //  Variant 3 move http in search component
 
   return (
     <div className={css.appWraper}>
       <SearchBar onInput={handleTopic} />
-     
-      {error&& <p>Whoops, something went wrong! Please try reloading this page!</p>}
-     
-      {listImages.length > 0 && <ImageGallery array={listImages} />}
       {loading && 
       <div className={css.barLoader}> <BarLoader/></div>}
-
-      {/* <p>{topic}</p> */}
+     
+      {error&& <ErrorMessage />}
+     
+      {listImages.length > 0 && <ImageGallery array={listImages} />}
+  
     </div>
   );
 
-  // const [count, setCount] = useState(0)
-
-  // return (
-  //   <>
-  //     <div>
-  //       <a href="https://vitejs.dev" target="_blank">
-  //         <img src={viteLogo} className="logo" alt="Vite logo" />
-  //       </a>
-  //       <a href="https://react.dev" target="_blank">
-  //         <img src={reactLogo} className="logo react" alt="React logo" />
-  //       </a>
-  //     </div>
-  //     <h1>Vite + React</h1>
-  //     <div className="card">
-  //       <button onClick={() => setCount((count) => count + 1)}>
-  //         count is {count}
-  //       </button>
-  //       <p>
-  //         Edit <code>src/App.jsx</code> and save to test HMR
-  //       </p>
-  //     </div>
-  //     <p className="read-the-docs">
-  //       Click on the Vite and React logos to learn more
-  //     </p>
-  //   </>
-  // )
+  
 }
 
 export default App;
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,33 +1,48 @@
-// import { CiSearch } from "react-icons/ci";
 import { IoSearchOutline } from 'react-icons/io5';
 import css from './SearchBar.module.css';
-import {useState} from "react"
-// import {useState} from "react"
+import { useState } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 
 export default function SearchBar({ onInput }) {
-  const[toast, setToast] = useState(false)
+  const [message, setMessage] = useState(false);
+
   const handleSubmit = event => {
     event.preventDefault();
     const form = event.target;
-    onInput(form.elements.topic.value);
-    if(form.elements.topic.value === "") {
-        setToast(true)
+    if (form.elements.topic.value.trim() === '') {
+      console.log(form.elements.topic.value.trim());
+      console.log(1);
+
+      setMessage(!false);
+      console.log(message);
+      toast('Here is your toast.');
+
+      return;
+    } else {
+      onInput(form.elements.topic.value);
+      console.log(2);
+      form.reset();
     }
-    form.reset();
   };
 
+ 
   return (
     <header className={css.searchBarHeader}>
       <div className={css.searchBarWraper}>
         <form className={css.searchBarForm} onSubmit={handleSubmit}>
-          <input className={css.searchBarInput} type="text" name="topic" placeholder="Search images and photos" autoFocus/>
+          <input
+            className={css.searchBarInput}
+            type="text"
+            name="topic"
+            placeholder="Search images and photos"
+            autoFocus
+          />
           <button className={css.searchBarButton}>
             <IoSearchOutline className={css.searchBarButtonIcon} />
           </button>
         </form>
-
       </div>
-      {toast && <p>Toast</p>} 
+      {message && <Toaster />}
     </header>
   );
 }
